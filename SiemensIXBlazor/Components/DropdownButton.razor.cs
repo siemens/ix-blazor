8e630117b6a20d6bf5235cccb109973a5125f5bd
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-02-15 16:54:25 +0300

    implement fropdown button

---
+++ SiemensIXBlazor/Components/DropdownButton.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Elements;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class DropdownButton
+    {
+        [Parameter]
+        public string Id { get; set; } = string.Empty;
+        [Parameter]
+        public bool Active { get; set; } = false;
+        [Parameter]
+        public bool Disabled { get; set; } = false;
+        [Parameter]
+        public bool Ghost { get; set; } = false;
+        [Parameter]
+        public string? Icon { get; set; }
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+        [Parameter]
+        public bool Outline { get; set; } = false;
+        [Parameter]
+        public string Variant { get; set; } = "Primary";
+        [Parameter]
+        public IEnumerable<DropdownItemElement>? DropdownItemElements { get; set; }
+        [Parameter]
+        public EventCallback<string> OnClickEvent { get; set; }
+
+        private async void Clicked(string label)
+        {
+            await OnClickEvent.InvokeAsync(label);
+        }
+    }
+}
---
+++ SiemensIXBlazor/Elements/DropdownItemElement.cs
@@ -0,0 +1,8 @@
+﻿namespace SiemensIXBlazor.Elements
+{
+    public class DropdownItemElement
+    {
+        public string Label { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
