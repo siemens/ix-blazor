85e1f2a12ae4c439317ed47b9b46452fc9482e23
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-02-23 20:29:40 +0300

    implement event list

---
+++ SiemensIXBlazor/Components/EventList.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class EventList
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public bool Animated { get; set; } = true;
+        [Parameter]
+        public bool? Chevron { get; set; }
+        [Parameter]
+        public bool Compact { get; set; } = false;
+        [Parameter]
+        public string ItemHeight { get; set; } = "S";
+    }
+}
---
+++ SiemensIXBlazor/Components/EventListItem.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using SiemensIXBlazor.Components.Interops;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json.Serialization;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class EventListItem
+    {
+        [Parameter]
+        public string Id { get; set; }
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+        [Parameter]
+        public bool? Chevron { get; set; }
+        [Parameter]
+        public string? Color { get; set; }
+        [Parameter]
+        public bool? Disabled { get; set; }
+        [Parameter]
+        public int Opacity { get; set; } = 1;
+        [Parameter]
+        public bool? Selected { get; set; }
+        [Parameter]
+        public EventCallback ItemCLickEvent { get; set; }
+
+        private BaseInterop _interop;
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _interop = new(JSRuntime);
+
+                await _interop.AddEventListener(this, Id, "itemClick", "ItemClicked");
+            }
+        }
+
+        [JSInvokable]
+        public async void ItemClicked()
+        {
+            await ItemCLickEvent.InvokeAsync();
+        }
+    }
+}
