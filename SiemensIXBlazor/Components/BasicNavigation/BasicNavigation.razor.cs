43c8fd81dd33aa9150ca87b946c623af28c98517
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-05-04 11:23:08 +0300

    implement BasicNavigation component

---
+++ SiemensIXBlazor/Components/BasicNavigation/BasicNavigation.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SiemensIXBlazor.Components.BasicNavigation
+{
+    public partial class BasicNavigation
+    {
+        [Parameter]
+        public string? ApplicationName { get; set; }
+        [Parameter]
+        public bool HideHeader { get; set; } = false;
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
