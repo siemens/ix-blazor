b113a3e3869a8a8829d627a0706113359fef9957
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-05-04 16:56:39 +0300

    implement about menu news component

---
+++ SiemensIXBlazor/Components/About/AboutMenuNews.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using SiemensIXBlazor.Interops;
+
+namespace SiemensIXBlazor.Components.About
+{
+    public partial class AboutMenuNews
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
+        [Parameter]
+        public string? AboutItemLabel { get; set; }
+        [Parameter]
+        public bool Expanded { get; set; } = false;
+        [Parameter]
+        public string I18NShowMore { get; set; } = "Show more";
+        [Parameter]
+        public string? Label { get; set; }
+        [Parameter]
+        public int OffsetBottom { get; set; } = 0;
+        [Parameter]
+        public bool Show { get; set; } = false;
+        [Parameter]
+        public EventCallback ClosePopoverEvent { get; set; }
+        [Parameter]
+        public EventCallback<MouseEventArgs> ShowMoreEvent { get; set; }
+
+        private BaseInterop _interop;
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _interop = new(JSRuntime);
+
+                await _interop.AddEventListener(this, Id, "closePopover", "ClosePopover");
+                await _interop.AddEventListener(this, Id, "showMore", "ShowMore");
+            }
+        }
+
+        [JSInvokable]
+        public async Task ClosePopover()
+        {
+            await ClosePopoverEvent.InvokeAsync();
+        }
+
+        [JSInvokable]
+        public async Task ShowMore(MouseEventArgs args)
+        {
+            await ShowMoreEvent.InvokeAsync(args);
+        }
+    }
+}
