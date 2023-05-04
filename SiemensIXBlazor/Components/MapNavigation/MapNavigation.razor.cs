9a25a5a3f39e4e4da8a24e95e18684dc2ec000f7
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-05-04 16:35:58 +0300

    implement map navigation component

---
+++ SiemensIXBlazor/Components/MapNavigation/MapNavigation.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SiemensIXBlazor.Interops;
+
+namespace SiemensIXBlazor.Components.MapNavigation
+{
+    public partial class MapNavigation
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
+        [Parameter]
+        public string? ApplicationName { get; set; }
+        [Parameter]
+        public bool HideContextMenu { get; set; } = true;
+        [Parameter]
+        public string? NavigationTitle { get; set; }
+        [Parameter]
+        public EventCallback ContextMenuClickEvent { get; set; }
+        [Parameter]
+        public EventCallback<bool> NavigationToggledEvent { get; set; }
+
+        private BaseInterop _interop;
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _interop = new(JSRuntime);
+
+                await _interop.AddEventListener(this, Id, "contextMenuClick", "ContextMenuClicked");
+                await _interop.AddEventListener(this, Id, "navigationToggled", "CollapsedChanged");
+            }
+        }
+
+        [JSInvokable]
+        public async Task ContextMenuClicked()
+        {
+            await ContextMenuClickEvent.InvokeAsync();
+        }
+
+        [JSInvokable]
+        public async Task NavigationToggled(bool status)
+        {
+            await NavigationToggledEvent.InvokeAsync(status);
+        }
+    }
+}
