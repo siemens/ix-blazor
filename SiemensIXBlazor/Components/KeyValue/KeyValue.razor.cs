d561a22e818789c88663c613ef9f97242401e887
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-06-29 01:18:07 +0200

    implement key-value and key-value-list components

---
+++ SiemensIXBlazor/Components/KeyValue/KeyValue.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace SiemensIXBlazor.Components
+{
+	public partial class KeyValue
+	{
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        /// <summary>
+        /// Optional key value icon
+        /// </summary>
+        [Parameter]
+		public string? Icon { get; set; }
+        /// <summary>
+        /// Key value label
+        /// </summary>
+        [Parameter]
+		public string? Label { get; set; }
+        /// <summary>
+        /// Optional key value label position - 'top' or 'left'
+        /// </summary>
+        [Parameter]
+		public string LabelPosition { get; set; } = "top";
+        /// <summary>
+        /// Optional key value text value
+        /// </summary>
+		[Parameter]
+		public string? Value { get; set; }
+	}
+}
+
---
+++ SiemensIXBlazor/Components/KeyValueList/KeyValueList.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace SiemensIXBlazor.Components
+{
+	public partial class KeyValueList
+	{
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        /// <summary>
+        /// Optional striped key value list style. Default value is null.
+        /// </summary>
+        [Parameter]
+        public bool? Striped { get; set; }
+    }
+}
+
