efb8e632893dda3bd3d8d6ff7eba8d092d0a5591
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-07-01 11:01:43 +0200

    implement action card

---
+++ SiemensIXBlazor/Components/ActionCard/ActionCard.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Enums.PushCard;
+
+namespace SiemensIXBlazor.Components
+{
+	public partial class ActionCard
+	{
+        /// <summary>
+        /// Card heading
+        /// </summary>
+        [Parameter]
+        public string? Heading { get; set; }
+        /// <summary>
+        /// Card icon
+        /// </summary>
+        [Parameter]
+        public string? Icon { get; set; }
+        /// <summary>
+        /// Card selection
+        /// </summary>
+        [Parameter]
+        public bool Selected { get; set; } = false;
+        /// <summary>
+        /// Card subheading
+        /// </summary>
+        [Parameter]
+        public string? SubHeading { get; set; }
+        /// <summary>
+        /// Card variant
+        /// </summary>
+        [Parameter]
+        public PushCardVariant Variant { get; set; } = PushCardVariant.Insight;
+    }
+}
+
