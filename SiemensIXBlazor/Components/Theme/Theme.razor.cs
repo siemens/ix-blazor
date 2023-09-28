5b29dfd5ddaf91d2640057a33cffc0cb40d75e86
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-09-28 16:49:13 +0300

    implement Theme component

---
+++ SiemensIXBlazor/Components/Theme/Theme.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class Theme
+    {
+        public async Task SetTheme(string theme)
+        {
+            Console.WriteLine(theme);
+            await JSRuntime.InvokeVoidAsync("setTheme", theme);
+        }
+
+        public async Task ToggleTheme()
+        {
+            await JSRuntime.InvokeVoidAsync("toggleTheme");
+        }
+
+        public async Task ToggleSystemTheme(bool useSystemTheme)
+        {
+            await JSRuntime.InvokeVoidAsync("toggleSystemTheme", useSystemTheme);
+        }
+    }
+}
--- SiemensIXBlazor/SiemensIXBlazor_NpmJS/src/index.js
+++ SiemensIXBlazor/SiemensIXBlazor_NpmJS/src/index.js
@@ -3,6 +3,7 @@ import { toast } from "@siemens/ix"
 import '@siemens/ix-echarts';
 import { registerTheme } from '@siemens/ix-echarts';
 import * as echarts from 'echarts';
+import { themeSwitcher } from '@siemens/ix';
 
 defineCustomElements();
 
@@ -11,17 +12,27 @@ window.showMessage = (config) => {
     const toastConfig = JSON.parse(config);
     toast(toastConfig);
 }
-
 // chart
 window.initializeChart = (id, options) => {
-
     registerTheme(echarts);
-
     var myChart = echarts.init(
         document.getElementById(id),
         window.demoTheme // brand-dark, brand-light, classic-dark or classic-light
     );
-
     // Draw the chart
     myChart.setOption(JSON.parse(options));
+}
+// set theme
+window.setTheme = (theme) => {
+    themeSwitcher.setTheme(theme);
+}
+// toggle theme
+window.toggleTheme = () => {
+    themeSwitcher.toggleMode();
+}
+// toggle system theme
+window.toggleSystemTheme = (useSystemTheme) => {
+    if (useSystemTheme === true) {
+        themeSwitcher.setVariant();
+    }
 }
