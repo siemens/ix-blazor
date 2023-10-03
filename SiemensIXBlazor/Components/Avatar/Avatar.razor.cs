8696df0d4426bf740a711e4d9a463de549ee9e19
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-03 21:34:10 +0300

    implement Avatar component

---
+++ SiemensIXBlazor/Components/Avatar/Avatar.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SiemensIXBlazor.Components.Avatar
+{
+    public partial class Avatar
+    {
+        [Parameter]
+        public string? Image { get; set; }
+        [Parameter]
+        public string? Initials { get; set; }
+    }
+}
