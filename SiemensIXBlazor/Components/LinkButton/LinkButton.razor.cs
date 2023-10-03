059fdc91cf92b46dd6bed32f72fbca46386a69b5
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-03 21:05:14 +0300

    add Parameter keyword

--- SiemensIXBlazor/Components/LinkButton/LinkButton.razor.cs
+++ SiemensIXBlazor/Components/LinkButton/LinkButton.razor.cs
@@ -1,11 +1,15 @@
-﻿using SiemensIXBlazor.Enums.LinkButton;
+﻿using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Enums.LinkButton;
 
 namespace SiemensIXBlazor.Components
 {
     public partial class LinkButton
     {
+        [Parameter]
         public bool Disabled { get; set; } = false;
+        [Parameter]
         public LinkButtonTarget Target { get; set; } = LinkButtonTarget._self;
+        [Parameter]
         public string? Url { get; set; }
     }
 }
