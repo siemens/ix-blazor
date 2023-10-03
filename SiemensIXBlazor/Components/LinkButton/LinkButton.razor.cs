f76693a18c9d009498bb060087f80b41f73ea36a
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-03 21:03:42 +0300

    implement LinkButton component

---
+++ SiemensIXBlazor/Components/LinkButton/LinkButton.razor.cs
@@ -0,0 +1,11 @@
+﻿using SiemensIXBlazor.Enums.LinkButton;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class LinkButton
+    {
+        public bool Disabled { get; set; } = false;
+        public LinkButtonTarget Target { get; set; } = LinkButtonTarget._self;
+        public string? Url { get; set; }
+    }
+}
---
+++ SiemensIXBlazor/Enums/LinkButton/LinkButtonTarget.cs
@@ -0,0 +1,10 @@
+﻿namespace SiemensIXBlazor.Enums.LinkButton
+{
+    public enum LinkButtonTarget
+    {
+        _blank,
+        _parent,
+        _self,
+        _top
+    }
+}
