ddb4a6a07fde1da2c242d10a24dffa7015d491fb
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-04 17:28:57 +0300

    implement slider component

---
+++ SiemensIXBlazor/Components/Slider/Slider.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SiemensIXBlazor.Interops;
+
+namespace SiemensIXBlazor.Components.Slider
+{
+    public partial class Slider
+    {
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public bool Disabled { get; set; } = false;
+        [Parameter]
+        public dynamic? Error { get; set; }
+        [Parameter]
+        public double[]? Marker { get; set; }
+        [Parameter]
+        public double Max { get; set; } = 100;
+        [Parameter]
+        public double Min { get; set; } = 0;
+        [Parameter]
+        public double? Step { get; set; }
+        [Parameter]
+        public bool Trace { get; set; } = false;
+        [Parameter]
+        public double TraceReference { get; set; } = 0;
+        [Parameter]
+        public double Value { get; set; } = 0;
+        [Parameter]
+        public EventCallback<double> ValueChangeEvent { get; set; }
+
+        private BaseInterop _interop;
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _interop = new(JSRuntime);
+
+                await _interop.AddEventListener(this, Id, "valueChange", "ValueChanged");
+            }
+        }
+
+        [JSInvokable]
+        public async void ValueChanged(double value)
+        {
+            await ValueChangeEvent.InvokeAsync(value);
+        }
+    }
+}
