3dd8620ce1d5368366e05e3d13865884723c1c45
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-04 15:17:53 +0300

    implement icon toggle button

---
+++ SiemensIXBlazor/Components/ToggleButton/IconToggleButton.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Enums.Button;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class IconToggleButton
+    {
+        [Parameter, EditorRequired]
+        public string Id { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public bool Disabled { get; set; } = false;
+        [Parameter]
+        public bool Ghost { get; set; } = false;
+        [Parameter]
+        public string? Icon { get; set; }
+        [Parameter]
+        public bool Loading { get; set; } = false;
+        [Parameter]
+        public bool Outline { get; set; } = false;
+        [Parameter]
+        public bool Pressed { get; set; } = false;
+        [Parameter]
+        public string Size { get; set; } = "24";
+        [Parameter]
+        public ButtonVariant Variant { get; set; } = ButtonVariant.secondary;
+        [Parameter]
+        public EventCallback<bool> PressedChangeEvent { get; set; }
+    }
+}
