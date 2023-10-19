1b8d0a747f872961ed5a9ae7e4970f34a774fa55
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-10-19 13:59:30 +0300

    implement AGGrid preview

---
+++ SiemensIXBlazor/Components/AGGrid/AGGrid.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+
+namespace SiemensIXBlazor.Components.AGGrid
+{
+    public partial class AGGrid
+    {
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
+
+        public async Task CreateGrid(GridOptions options)
+        {
+            if (Id == string.Empty)
+            {
+                return;
+            }
+
+            await JSRuntime.InvokeVoidAsync("agGridInterop.createGrid", Id, JsonConvert.SerializeObject(options));
+        }
+    }
+}
---
+++ SiemensIXBlazor/Components/AGGrid/GridOptions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace SiemensIXBlazor.Components.AGGrid
+{
+    public class GridOptions
+    {
+        [JsonProperty("columnDefs")]
+        public List<ColumnDefs>? ColumnDefs { get; set; }
+        [JsonProperty("rowData")]
+        public List<Dictionary<string, dynamic>>? RowData { get; set; }
+        [JsonProperty("rowSelection")]
+        public string? RowSelection { get; set; }
+        [JsonProperty("suppressCellFocus")]
+        public bool? SuppressCellFocus { get; set; }
+        [JsonProperty("checkboxSelection")]
+        public bool? CheckboxSelection { get; set; }
+    }
+}
