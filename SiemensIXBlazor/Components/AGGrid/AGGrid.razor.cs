4e54d47258c1eea469e9f48850388f502e5d1922
Adam Hacsko <adam.hacsko@evosoft.com>
2024-03-13 09:13:22 +0100

    extend aggrid

--- SiemensIXBlazor/Components/AGGrid/AGGrid.razor.cs
+++ SiemensIXBlazor/Components/AGGrid/AGGrid.razor.cs
@@ -4,19 +4,38 @@ using Newtonsoft.Json;
 
 namespace SiemensIXBlazor.Components.AGGrid
 {
-	public partial class AGGrid
-	{
-		[Parameter, EditorRequired]
-		public string Id { get; set; } = string.Empty;
+    public partial class AGGrid
+    {
+        [Parameter]
+        public EventCallback OnCellClicked { get; set; }
 
-		public async Task<IJSObjectReference?> CreateGrid(GridOptions options)
-		{
-			if (Id == string.Empty)
-			{
-				return null;
-			}
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
 
-			return await JSRuntime.InvokeAsync<IJSObjectReference?>("agGridInterop.createGrid", Id, JsonConvert.SerializeObject(options));
-		}
-	}
+        private DotNetObjectReference<AGGrid>? dotNetHelper;
+
+        public async Task<IJSObjectReference?> CreateGrid(GridOptions options)
+        {
+            if (Id == string.Empty)
+            {
+                return null;
+            }
+
+            dotNetHelper = DotNetObjectReference.Create(this);
+
+            return await JSRuntime.InvokeAsync<IJSObjectReference?>("agGridInterop.createGrid", dotNetHelper, Id, JsonConvert.SerializeObject(options));
+        }
+
+        public async Task<object?> GetSelectedRows(IJSObjectReference gird)
+        {
+            return await JSRuntime.InvokeAsync<Object>("agGridInterop.getSelectedRows", gird);
+            
+        }
+
+        [JSInvokable]
+        public async Task OnCellClickedCallback()
+        {
+            await OnCellClicked.InvokeAsync();
+        }
+    }
 }
