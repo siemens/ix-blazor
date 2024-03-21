ac5fea4dca0d4f0f64cb68d8ab2b8772fda14e4c
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-03-21 15:29:01 +0300

    add application header component

---
+++ SiemensIXBlazor/Components/ApplicationHeader/ApplicationHeader.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SiemensIXBlazor.Components
+{
+    public partial class ApplicationHeader
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public string? Name { get; set; }
+    }
+}
