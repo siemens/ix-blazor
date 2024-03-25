7f59d5d3049d5d7cf35353c3270a44c3f40303b2
Emre Gokrem <emre.gokrem@siemens.com>
2024-03-25 16:50:10 +0300

    DataDropdown component added

---
+++ SiemensIXBlazor/Objects/DateDropdown/DateDropdownOption.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace SiemensIXBlazor.Objects.DateDropdown;
+
+public class
+    DateDropdownOption
+{
+    [JsonProperty("id")] public string Id { get; set; } = null!;
+
+    [JsonProperty("label")] public string Label { get; set; } = null!;
+
+    [JsonProperty("from")] public string? From { get; set; }
+
+    [JsonProperty("to")] public string? To { get; set; }
+}
---
+++ SiemensIXBlazor/wwwroot/js/interops/dateDropdownInterop.js
@@ -0,0 +1,9 @@
+﻿export function setDateRangeOptions(id, dateRangeOptions) {
+    try {
+        const element = document.getElementById(id);
+        element.dateRangeOptions = JSON.parse(dateRangeOptions);
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
