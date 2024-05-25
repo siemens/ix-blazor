477f5be7e47ce3586bd5050ffd26b940a6cd5994
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-05-25 17:08:04 +0300

    chore: Add unit tests for ApplicationHeader component

---
+++ SiemensIXBlazor.Tests/ApplicationHeaderTests.cs
@@ -0,0 +1,37 @@
+using Bunit;
+using SiemensIXBlazor.Components;
+
+namespace SiemensIXBlazor.Tests
+{
+    public class ApplicationHeaderTests : TestContextBase
+    {
+        [Fact]
+        public void ApplicationRendersWithoutCrashing()
+        {
+            // Arrange
+            var cut = RenderComponent<ApplicationHeader>(parameters => {
+                parameters.Add(p => p.Name, "testName");
+            });
+
+            // Assert
+            cut.MarkupMatches("<ix-application-header name='testName'></ix-application-header>");
+        }
+
+        [Fact]
+        public void ApplicationHeaderRendersChildContent()
+        {
+            // Arrange
+            var expectedContent = "Expected content";
+
+            // Act
+            var cut = RenderComponent<ApplicationHeader>(parameters => parameters
+                .Add(p => p.ChildContent, builder => 
+                {
+                    builder.AddContent(0, expectedContent);
+                }));
+
+            // Assert
+            Assert.Contains(expectedContent, cut.Markup);
+        }
+    }
+}
