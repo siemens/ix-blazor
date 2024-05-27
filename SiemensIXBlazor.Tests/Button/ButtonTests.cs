7046db551657686a084029a3192585d03788e980
Yagizhan Necat Yakali <yagizhanyakali@gmail.com>
2024-05-27 16:55:37 +0300

    test: Add unit tests for Button component

---
+++ SiemensIXBlazor.Tests/Button/ButtonTests.cs
@@ -0,0 +1,47 @@
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Components;
+using SiemensIXBlazor.Enums.Button;
+
+namespace SiemensIXBlazor.Tests
+{
+    public class ButtonTests: TestContextBase
+    {
+        [Fact]
+        public void ButtonRendersWithoutCrashing()
+        {
+            // Arrange
+            var cut = RenderComponent<Button>(parameters => {
+                parameters.Add(p => p.Id, "testId");
+                parameters.Add(p => p.DataToggle, "testDataToggle");
+                parameters.Add(p => p.Variant, ButtonVariant.primary);
+                parameters.Add(p => p.Disabled, true);
+                parameters.Add(p => p.Ghost, true);
+                parameters.Add(p => p.Icon, "testIcon");
+                parameters.Add(p => p.Loading, true);
+                parameters.Add(p => p.Outline, true);
+                parameters.Add(p => p.Selected, true);
+                parameters.Add(p => p.Type, ButtonType.Button);
+                parameters.Add(p => p.DataTooltip, "testDataTooltip");
+            });
+        
+            // Assert
+            cut.MarkupMatches("<ix-button  id='testId' disabled='' ghost='' outline='' selected='' icon='testIcon' loading='' type='button' variant='primary' data-toggle='testDataToggle' data-tooltip='testDataTooltip'></ix-button>");
+        }
+
+        [Fact]
+        public void ButtonComponentHandlesClickEvent()
+        {
+            // Arrange
+            var clickInvoked = false;
+            var cut = RenderComponent<Button>(parameters => parameters
+                .Add(p => p.ClickEvent, EventCallback.Factory.Create(this, () => clickInvoked = true)));
+
+            // Act
+            cut.Find("ix-button").Click();
+
+            // Assert
+            Assert.True(clickInvoked);
+        }
+    }
+}
