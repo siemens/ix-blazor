44528136e89f3aeb01318df5d4b2e05f9d44ae88
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-05-27 17:14:40 +0300

    test: add IconButton unit tests.

---
+++ SiemensIXBlazor.Tests/Button/IconButtonTests.cs
@@ -0,0 +1,49 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Components;
+using SiemensIXBlazor.Enums.Button;
+
+namespace SiemensIXBlazor.Tests
+{
+    public class IconButtonTests: TestContextBase
+    {
+        [Fact]
+        public void ButtonRendersWithoutCrashing()
+        {
+            // Arrange
+            var cut = RenderComponent<IconButton>(parameters => {
+                parameters.Add(p => p.IconColor, "testIconColor");
+                parameters.Add(p => p.Disabled, true);
+                parameters.Add(p => p.Ghost, true);
+                parameters.Add(p => p.Icon, "testIcon");
+                parameters.Add(p => p.Loading, true);
+                parameters.Add(p => p.Outline, true);
+                parameters.Add(p => p.Oval, true);
+                parameters.Add(p => p.Size, IconButtonSize._12);
+                parameters.Add(p => p.Selected, true);
+                parameters.Add(p => p.DataTooltip, "testDataTooltip");
+                parameters.Add(p => p.Type, ButtonType.Button);
+                parameters.Add(p => p.A11yLabel, "testA11Label");
+                parameters.Add(p => p.Variant, ButtonVariant.primary);
+            });
+
+            // Assert
+            cut.MarkupMatches("<ix-icon-button  disabled=\"\" ghost=\"\" outline=\"\" selected=\"\" type=\"button\" variant=\"primary\" icon-color=\"testIconColor\" icon=\"testIcon\" oval=\"\" loading=\"\" data-tooltip=\"testDataTooltip\" size=\"12\" a11y-label=\"testA11Label\"></ix-icon-button>");
+        }
+
+        [Fact]
+        public void ButtonComponentHandlesClickEvent()
+        {
+            // Arrange
+            var clickInvoked = false;
+            var cut = RenderComponent<IconButton>(parameters => parameters
+                .Add(p => p.ClickEvent, EventCallback.Factory.Create(this, () => clickInvoked = true)));
+
+            // Act
+            cut.Find("ix-icon-button").Click();
+
+            // Assert
+            Assert.True(clickInvoked);
+        }
+    }
+}
