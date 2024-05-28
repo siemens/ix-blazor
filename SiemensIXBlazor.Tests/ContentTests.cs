ef7ab953eae02b0652992aafac719d8d2e2ae2a9
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-05-28 17:41:15 +0300

    test: add Content component's unit tests.

---
+++ SiemensIXBlazor.Tests/ContentTests.cs
@@ -0,0 +1,24 @@
+﻿using SiemensIXBlazor.Components;
+
+namespace SiemensIXBlazor.Tests
+{
+    public class ContentTests: TestContextBase
+    {
+        [Fact]
+        public void ContentRendersChildContent()
+        {
+            // Arrange
+            var expectedContent = "Expected content";
+
+            // Act
+            var cut = RenderComponent<Content>(parameters => parameters
+                .Add(p => p.ChildContent, builder =>
+                {
+                    builder.AddContent(0, expectedContent);
+                }));
+
+            // Assert
+            Assert.Contains(expectedContent, cut.Markup);
+        }
+    }
+}
