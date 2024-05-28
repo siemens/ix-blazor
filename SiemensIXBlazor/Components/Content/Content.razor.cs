c1c00f27f769b1464db57d7b46759b34be4bdc13
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-05-28 17:41:36 +0300

    chor: remove unused property from Content component.

--- SiemensIXBlazor/Components/Content/Content.razor.cs
+++ SiemensIXBlazor/Components/Content/Content.razor.cs
@@ -14,7 +14,5 @@ namespace SiemensIXBlazor.Components;
 public partial class Content
 {
     [Parameter]
-    public RenderFragment? HeaderContent { get; set; }
-    [Parameter]
     public RenderFragment? ChildContent { get; set; }
 }
