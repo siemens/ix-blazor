051c8052f8c8c1744c7d1d850351ec96a6b55eb1
emregokrem <gokrememre@gmail.com>
2024-05-30 11:23:33 +0300

    Added the header for the missing files

--- SiemensIXBlazor.Tests/ContentHeaderTests.cs
+++ SiemensIXBlazor.Tests/ContentHeaderTests.cs
@@ -1,4 +1,13 @@
-﻿using Bunit;
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2024 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//  -----------------------------------------------------------------------
+
+using Bunit;
 using Microsoft.AspNetCore.Components;
 using SiemensIXBlazor.Components;
 using SiemensIXBlazor.Components.Avatar;
