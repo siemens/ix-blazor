1ab72107f5ea679aa64669b55f5ef87736e9da34
Emin Can ÖZGE <emincanozge@outlook.com>
2024-09-25 14:44:34 +0300

    feat: Implement Typography component

---
+++ SiemensIXBlazor/Enums/Typography/TextDecoration.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2024 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//  -----------------------------------------------------------------------
+
+namespace SiemensIXBlazor.Enums.Typography;
+
+public enum TextDecoration
+{
+	None,
+	Underline,
+	Line_Through
+}
---
+++ SiemensIXBlazor/Enums/Typography/TypographyFormat.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2024 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//  -----------------------------------------------------------------------
+
+namespace SiemensIXBlazor.Enums.Typography;
+
+public enum TypographyFormat
+{
+	H1,
+	H2,
+	H3,
+	H4,
+	H5,
+	H6,
+	Body,
+	Body_Xs,
+	Body_Sm,
+	Body_Lg,
+	Code,
+	Code_Sm,
+	Code_Lg,
+	Display,
+	Display_Xs,
+	Display_Sm,
+	Display_Lg,
+	Display_Xl,
+	Display_Xxl,
+	Label,
+	Label_Xs,
+	Label_Sm,
+	Label_Lg
+}
