8823bcfc24f691f558d858c8740c214f44663848
Yagizhan Yakali <yagizhan.yakali@on-running.com>
2024-09-28 20:39:50 +0200

    Improve pipeline

--- SiemensIXBlazor/wwwroot/js/siemens-ix/7262.index.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknpmjs=self.webpackChunknpmjs||[]).push([[7262],{7262:(e,s,t)=>{t.r(s),t.d(s,{ix_flip_tile_content:()=>c});var n=t(6969);const c=class{constructor(e){(0,n.r)(this,e),this.contentVisible=!1}render(){return(0,n.h)(n.H,{key:"e92abcbb8b1cfdeb739ee4933f6ed66a6c2a7dc5"},this.contentVisible?(0,n.h)("slot",null):null)}};c.style=":host{display:block}"}}]);
