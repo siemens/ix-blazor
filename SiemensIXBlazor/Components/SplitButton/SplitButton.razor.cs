8f20e376483373c161909858ea1b422ed232cb3e
Emin Can ÖZGE <emincanozge@outlook.com>
2024-11-20 15:46:29 +0300

    Add SplitButton close behavior and update test

--- SiemensIXBlazor/Components/SplitButton/SplitButton.razor.cs
+++ SiemensIXBlazor/Components/SplitButton/SplitButton.razor.cs
@@ -8,54 +8,54 @@
 //  -----------------------------------------------------------------------
 
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using SiemensIXBlazor.Enums.Button;
 using SiemensIXBlazor.Interops;
 
-namespace SiemensIXBlazor.Components
+namespace SiemensIXBlazor.Components;
+
+public partial class SplitButton
 {
-    public partial class SplitButton
-    {
-        [Parameter]
-        public RenderFragment? ChildContent { get; set; }
-        [Parameter, EditorRequired]
-        public string Id { get; set; } = string.Empty;
-        [Parameter]
-        public bool Disabled { get; set; } = false;
-        [Parameter]
-        public bool Ghost { get; set; } = false;
-        [Parameter]
-        public string Icon { get; set; } = string.Empty;
-        [Parameter]
-        public string? Label { get; set; }
-        [Parameter]
-        public bool Outline { get; set; } = false;
-        [Parameter]
-        public string Placement { get; set; } = "bottom-start";
-        [Parameter]
-        public string SplitIcon { get; set; } = "context-menu";
-        [Parameter]
-        public ButtonVariant Variant { get; set; } = ButtonVariant.primary;
-        [Parameter]
-        public EventCallback ButtonClickedEvent { get; set; }
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+	[Parameter, EditorRequired]
+	public string Id { get; set; } = string.Empty;
+	[Parameter]
+	public bool Disabled { get; set; } = false;
+	[Parameter]
+	public bool Ghost { get; set; } = false;
+	[Parameter]
+	public string Icon { get; set; } = string.Empty;
+	[Parameter]
+	public string? Label { get; set; }
+	[Parameter]
+	public bool Outline { get; set; } = false;
+	[Parameter]
+	public string Placement { get; set; } = "bottom-start";
+	[Parameter]
+	public string SplitIcon { get; set; } = "context-menu";
+	[Parameter]
+	public CloseBehavior CloseBehavior { get; set; } = CloseBehavior.Both;
+	[Parameter]
+	public ButtonVariant Variant { get; set; } = ButtonVariant.primary;
+	[Parameter]
+	public EventCallback ButtonClickedEvent { get; set; }
 
-        private BaseInterop _interop;
+	private BaseInterop _interop;
 
-        protected async override Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                _interop = new(JSRuntime);
+	protected async override Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender)
+		{
+			_interop = new(JSRuntime);
 
-                await _interop.AddEventListener(this, Id, "buttonClick", "ButtonClicked");
-            }
-        }
+			await _interop.AddEventListener(this, Id, "buttonClick", "ButtonClicked");
+		}
+	}
 
-        [JSInvokable]
-        public async void ButtonClicked()
-        {
-            await ButtonClickedEvent.InvokeAsync();
-        }
-    }
+	[JSInvokable]
+	public async void ButtonClicked()
+	{
+		await ButtonClickedEvent.InvokeAsync();
+	}
 }
---
+++ SiemensIXBlazor/Enums/Button/SplitButtonCloseBehavior.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2024 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//  -----------------------------------------------------------------------
+
+namespace SiemensIXBlazor.Enums.Button;
+
+public enum CloseBehavior
+{
+	Both,
+	Inside,
+	Outside,
+	True,
+	False
+}
