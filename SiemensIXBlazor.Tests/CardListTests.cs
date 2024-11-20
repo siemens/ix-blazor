600a258053a7fb6472f0e1704db512901148d831
Emin Can ÖZGE <emincanozge@outlook.com>
2024-11-20 15:47:13 +0300

    Add HideShowAll property to CardList and update tests

--- SiemensIXBlazor.Tests/CardListTests.cs
+++ SiemensIXBlazor.Tests/CardListTests.cs
@@ -13,67 +13,70 @@ using SiemensIXBlazor.Components;
 
 namespace SiemensIXBlazor.Tests
 {
-    public class CardListTests: TestContextBase
-    {
-        [Fact]
-        public void CardListRendersWithoutCrashing()
-        {
-            // Arrange
-            var cut = RenderComponent<CardList>(parameters => {
-                parameters.Add(p => p.Id, "testId");
-                parameters.Add(p => p.Collapse, true);
-                parameters.Add(p => p.I18NMoreCards, "testMoreCards");
-                parameters.Add(p => p.I18NShowAll, "testShowAll");
-                parameters.Add(p => p.Label, "testLabel");
-                parameters.Add(p => p.ListStyle, Enums.CardList.CardListStyle.Stack);
-                parameters.Add(p => p.ShowAllCount, 1);
-                parameters.Add(p => p.SuppressOverflowHandling, true);
-            });
+	public class CardListTests : TestContextBase
+	{
+		[Fact]
+		public void CardListRendersWithoutCrashing()
+		{
+			// Arrange
+			var cut = RenderComponent<CardList>(parameters =>
+			{
+				parameters.Add(p => p.Id, "testId");
+				parameters.Add(p => p.Collapse, true);
+				parameters.Add(p => p.I18NMoreCards, "testMoreCards");
+				parameters.Add(p => p.I18NShowAll, "testShowAll");
+				parameters.Add(p => p.Label, "testLabel");
+				parameters.Add(p => p.ListStyle, Enums.CardList.CardListStyle.Stack);
+				parameters.Add(p => p.ShowAllCount, 1);
+				parameters.Add(p => p.SuppressOverflowHandling, true);
+				parameters.Add(p => p.HideShowAll, true);
 
-            // Assert
-            cut.MarkupMatches("<ix-card-list id=\"testId\" label=\"testLabel\" show-all-count=\"1\" list-style=\"stack\" collapse=\"\" i-1-8n-more-cards=\"testMoreCards\" i-1-8n-show-all=\"testShowAll\" suppress-overflow-handling=\"\"></ix-card-list>");
-        }
+			});
 
-        [Fact]
-        public void CollapsedChangedEventTriggeredCorrectly()
-        {
-            // Arrange
-            var eventTriggered = false;
-            var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.CollapseChangedEvent, EventCallback.Factory.Create<bool>(this, () => eventTriggered = true)));
+			// Assert
+			cut.MarkupMatches("<ix-card-list id=\"testId\" label=\"testLabel\" show-all-count=\"1\" list-style=\"stack\" collapse=\"\" i-1-8n-more-cards=\"testMoreCards\" i-1-8n-show-all=\"testShowAll\" suppress-overflow-handling=\"\" hide-show-all></ix-card-list>");
+		}
 
-            // Act
-            cut.Instance.CollapseChangedEvent.InvokeAsync(true);
+		[Fact]
+		public void CollapsedChangedEventTriggeredCorrectly()
+		{
+			// Arrange
+			var eventTriggered = false;
+			var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.CollapseChangedEvent, EventCallback.Factory.Create<bool>(this, () => eventTriggered = true)));
 
-            // Assert
-            Assert.True(eventTriggered);
-        }
+			// Act
+			cut.Instance.CollapseChangedEvent.InvokeAsync(true);
 
-        [Fact]
-        public void ShowAllClickedEventTriggeredCorrectly()
-        {
-            // Arrange
-            var eventTriggered = false;
-            var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.ShowAllClickEvent, EventCallback.Factory.Create(this, () => eventTriggered = true)));
+			// Assert
+			Assert.True(eventTriggered);
+		}
 
-            // Act
-            cut.Instance.ShowAllClickEvent.InvokeAsync();
+		[Fact]
+		public void ShowAllClickedEventTriggeredCorrectly()
+		{
+			// Arrange
+			var eventTriggered = false;
+			var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.ShowAllClickEvent, EventCallback.Factory.Create(this, () => eventTriggered = true)));
 
-            // Assert
-            Assert.True(eventTriggered);
-        }
+			// Act
+			cut.Instance.ShowAllClickEvent.InvokeAsync();
 
-        [Fact]
-        public void ShowMoreCardClickedEventTriggeredCorrectly()
-        {
-            // Arrange
-            var eventTriggered = false;
-            var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.ShowMoreCardClickEvent, EventCallback.Factory.Create(this, () => eventTriggered = true)));
+			// Assert
+			Assert.True(eventTriggered);
+		}
 
-            // Act
-            cut.Instance.ShowMoreCardClickEvent.InvokeAsync();
+		[Fact]
+		public void ShowMoreCardClickedEventTriggeredCorrectly()
+		{
+			// Arrange
+			var eventTriggered = false;
+			var cut = RenderComponent<CardList>(parameters => parameters.Add(p => p.ShowMoreCardClickEvent, EventCallback.Factory.Create(this, () => eventTriggered = true)));
 
-            // Assert
-            Assert.True(eventTriggered);
-        }
-    }
+			// Act
+			cut.Instance.ShowMoreCardClickEvent.InvokeAsync();
+
+			// Assert
+			Assert.True(eventTriggered);
+		}
+	}
 }
