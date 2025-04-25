7a836b7db3119ed199c21229db631a238d545cd2
Emin Can ÖZGE <emincanozge@outlook.com>
2025-04-25 14:22:57 +0300

    feat(slider): add marker feature as interop

---
+++ SiemensIXBlazor/wwwroot/js/siemens-ix/interops/sliderInterop.js
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2025 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//  -----------------------------------------------------------------------
+
+export function setMarker(id, markerArray) {
+    const el = document.getElementById(id);
+    if (el) {
+        el.marker = markerArray;
+    }
+}
+
+export function listenEvent(dotNetRef, id, eventName, callbackName) {
+    const el = document.getElementById(id);
+    if (el) {
+        el.addEventListener(eventName, e => {
+            if (callbackName === "ValueChanged") {
+                dotNetRef.invokeMethodAsync(callbackName, e.detail);
+            }
+        });
+    }
+}
