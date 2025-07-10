88eeb941baad6c0df3d489073d8a0dbc1e9c764e
Mustafa Kurubal <mustafakurubal34@gmail.com>
2025-07-10 16:47:39 +0300

    Add license text to DateDropdown

--- SiemensIXBlazor.Playground/Components/Pages/InputFieldsAndSelections/DateDropdown/DateDropdown.razor.cs
+++ SiemensIXBlazor.Playground/Components/Pages/InputFieldsAndSelections/DateDropdown/DateDropdown.razor.cs
@@ -1,4 +1,13 @@
-﻿using SiemensIXBlazor.Objects.DateDropdown;
+﻿// -----------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2025 Siemens AG
+//
+// SPDX-License-Identifier: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+// -----------------------------------------------------------------------
+
+using SiemensIXBlazor.Objects.DateDropdown;
 
 namespace SiemensIXBlazor.Playground.Components.Pages.InputFieldsAndSelections.DateDropdown
 {
