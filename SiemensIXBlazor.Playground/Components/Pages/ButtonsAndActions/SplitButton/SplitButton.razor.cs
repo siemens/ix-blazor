d5015b753e9081b42fef562594b30dd6cae0cc4d
Mustafa Kurubal <mustafakurubal34@gmail.com>
2025-07-22 11:47:01 +0300

    Add Playground components for SplitButton

--- SiemensIXBlazor.Playground/Components/Pages/ButtonsAndActions/SplitButton/SplitButton.razor.cs
+++ SiemensIXBlazor.Playground/Components/Pages/ButtonsAndActions/SplitButton/SplitButton.razor.cs
@@ -12,4 +12,20 @@ namespace SiemensIXBlazor.Playground.Components.Pages.ButtonsAndActions.SplitBut
 public partial class SplitButton
 {
     private int activeTab = 0;
+
+    public string ContentForBasic { get; private set; } = @"
+        <SplitButton Id=""split-button-1""
+                 Label=""Split Button""
+                 SplitIcon=""chevron-down-small""
+                ButtonClickedEvent=""SplitButtonClicked"">
+            <DropdownItem Label=""Item 1""></DropdownItem>
+            <DropdownItem Label=""Item 2""></DropdownItem>
+        </SplitButton>";
+    public string ContentForIconOnly { get; private set; } = @"
+        <SplitButton Id=""split-button-1""
+                 SplitIcon=""chevron-down-small""
+                ButtonClickedEvent=""SplitButtonClicked"">
+            <DropdownItem Icon=""Cut""></DropdownItem>
+            <DropdownItem Icon=""Bulb""></DropdownItem>
+        </SplitButton>";
 }
