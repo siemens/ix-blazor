12024bcb2043f4a7b3a4aa9400e90e7499bbe011
Ayse Sezin Gursoy <e210503051@stud.tau.edu.tr>
2025-08-22 12:59:16 +0300

    add: Key Value, Key Value List, KPI

--- SiemensIXBlazor.Playground/Components/Pages/DataDisplay/KPI/KPI.razor.cs
+++ SiemensIXBlazor.Playground/Components/Pages/DataDisplay/KPI/KPI.razor.cs
@@ -12,4 +12,17 @@ namespace SiemensIXBlazor.Playground.Components.Pages.DataDisplay.KPI;
 public partial class KPI
 {
     private int activeTab = 0;
+
+    public string ContentForBasic { get; private set; } = @"
+        <div class=""example"">
+            <div class=""kpi"">
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-1"" Label=""Motor Speed"" Value=""Nominal""></SiemensIXBlazor.Components.KPI>
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-2"" Label=""Motor Speed"" Value=""122.6"" Unit=""rpm""></SiemensIXBlazor.Components.KPI>
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-3"" Label=""Motor Speed"" Value=""122.6"" State=""Enums.KPI.KpiState.Alarm""></SiemensIXBlazor.Components.KPI>
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-4"" Label=""Motor Speed"" Value=""122.6"" State=""Enums.KPI.KpiState.Warning""></SiemensIXBlazor.Components.KPI>
+
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-5"" Label=""Motor Speed"" Value=""Nominal"" Orientation=""Enums.KPI.KpiOrientation.Vertical""></SiemensIXBlazor.Components.KPI>
+                <SiemensIXBlazor.Components.KPI id=""basic-kpi-6"" Label=""Motor Speed"" Value=""122.6"" Unit=""rpm"" State=""Enums.KPI.KpiState.Alarm"" Orientation=""Enums.KPI.KpiOrientation.Vertical""></SiemensIXBlazor.Components.KPI>
+            </div>
+        </div>";
 }
--- SiemensIXBlazor.Playground/Components/Pages/DataDisplay/KeyValue/KeyValue.razor.cs
+++ SiemensIXBlazor.Playground/Components/Pages/DataDisplay/KeyValue/KeyValue.razor.cs
@@ -12,4 +12,22 @@ namespace SiemensIXBlazor.Playground.Components.Pages.DataDisplay.KeyValue;
 public partial class KeyValue
 {
     private int activeTab = 0;
+    public string ContentForBasic { get; private set; } = @"
+        <SiemensIXBlazor.Components.KeyValue Id=""basic-key-value"" Label=""Label"" Value=""Value""></SiemensIXBlazor.Components.KeyValue>";
+
+    public string ContentForCustomValue { get; private set; } = @"
+        <SiemensIXBlazor.Components.KeyValue Id=""custom-key-value"" Label=""Label"">
+            <input class=""ix-form-control""
+                   placeholder=""Enter text here""
+                   type=""text""
+                   slot=""custom-value"" />
+        </SiemensIXBlazor.Components.KeyValue>";
+
+    public string ContentForWithIcon { get; private set; } = @"
+        <SiemensIXBlazor.Components.KeyValue Icon=""location"" Label=""Label"" Value=""Value"">
+        </SiemensIXBlazor.Components.KeyValue>";
+
+    public string ContentForLabelOnLeft { get; private set; } = @"
+        <SiemensIXBlazor.Components.KeyValue Id=""left-key-value"" Label=""Label"" LabelPosition=""left"" Value=""Value"">
+        </SiemensIXBlazor.Components.KeyValue>";
 }
