b7789a50b4f6f0d09e6092e24cedede83343805f
Emin Can ÖZGE <emincanozge@outlook.com>
2026-08-14 17:00:52 +0300

    feat(layout): type column sizes and add layout auto (#294)
    - Add typed column sizes, grid gaps, and LayoutAuto configuration
    - Align layout defaults and serialized object names with IX
    - Update tests and README

--- SiemensIXBlazor.Tests/LayoutGrid/ColTest.cs
+++ SiemensIXBlazor.Tests/LayoutGrid/ColTest.cs
@@ -9,6 +9,7 @@
 
 using Bunit;
 using Microsoft.AspNetCore.Components;
+using SiemensIXBlazor.Enums.LayoutGrid;
 using SiemensIXBlazor.Components.LayoutGrid;
 
 namespace SiemensIXBlazor.Tests.LayoutGrid;
@@ -21,12 +22,29 @@ public class ColTest : TestContextBase
         // Arrange
         var cut = RenderComponent<Col>(parameters => parameters
             .Add(p => p.ChildContent, (RenderFragment)(builder => builder.AddMarkupContent(0, "Test content")))
-            .Add(p => p.Size, "12")
-            .Add(p => p.SizeLg, "10")
-            .Add(p => p.SizeMd, "8")
-            .Add(p => p.SizeSm, "6"));
+            .Add(p => p.Size, ColumnSize._12)
+            .Add(p => p.SizeLg, ColumnSize._10)
+            .Add(p => p.SizeMd, ColumnSize._8)
+            .Add(p => p.SizeSm, ColumnSize._6));
 
         // Assert
         cut.MarkupMatches("<ix-col size=\"12\" size-lg=\"10\" size-md=\"8\" size-sm=\"6\">Test content</ix-col>");
+    }
+
+    [Fact]
+    public void ComponentRendersWithoutSizeAttributesByDefault()
+    {
+        var cut = RenderComponent<Col>();
+
+        cut.MarkupMatches("<ix-col></ix-col>");
+    }
+
+    [Fact]
+    public void ComponentRendersAutoColumnSize()
+    {
+        var cut = RenderComponent<Col>(parameters => parameters
+            .Add(p => p.Size, ColumnSize.auto));
+
+        cut.MarkupMatches("<ix-col size=\"auto\"></ix-col>");
     }
 }
